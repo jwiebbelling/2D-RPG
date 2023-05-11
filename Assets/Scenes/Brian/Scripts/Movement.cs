7e76951ae6d45f45a1acfd4ed4559947822bbdf0
C-h-a-o-z <b.somers@sintlucasedu.nl>
2023-05-11 14:35:52 +0200

    basics movement down

--- Assets/Scenes/Brian/Scripts/Movement.cs
+++ Assets/Scenes/Brian/Scripts/Movement.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    [SerializeField] KeyCode up= KeyCode.W;
+    [SerializeField] KeyCode up = KeyCode.W;
     [SerializeField] KeyCode down = KeyCode.S;
     [SerializeField] KeyCode left = KeyCode.A;
     [SerializeField] KeyCode right = KeyCode.D;
-    public float speed = 100;
+    public float speed = 5;
+    float ySpeed;
+    float xSpeed;
+    float yMult = 1f;
+    float xMult = 1f;
     Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
@@ -19,23 +23,39 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(Time.deltaTime);
         if (Input.GetKey(up))
         {
-            rb.velocity = new Vector2(rb.velocity.x, speed * Time.deltaTime) ;
+            ySpeed = speed;
+            xMult = 0.7f;
+        }
+        else if (Input.GetKey(down))
+        {
+            ySpeed = 0 - speed;
+            xMult = 0.7f;
         }
         else
         {
-            rb.velocity = new Vector2(rb.velocity.x, 0); //this stops it from working
+            ySpeed = 0;
+            xMult = 1f;
+            //rb.velocity = new Vector2(rb.velocity.x, 0); //this stops it from working
         }
 
-        if (Input.GetKey(down))
+        if (Input.GetKey(right))
         {
-            rb.velocity = new Vector2(rb.velocity.x, 0 - speed * Time.deltaTime);
+            xSpeed = speed;
+            yMult = 0.7f;
+        }
+        else if (Input.GetKey(left))
+        {
+            xSpeed = 0 - speed;
+            yMult = 0.7f;
         }
         else
         {
-            rb.velocity = new Vector2(rb.velocity.x, 0); //this stops it from working
+            xSpeed = 0;
+            yMult = 1f;
         }
+
+        rb.velocity = new Vector2(xSpeed * xMult, ySpeed * yMult);
     }
 }
