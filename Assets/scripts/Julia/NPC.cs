105ac0397ecdb6da0582f6e738d3ba470c3cb02e
j.wiebbelling <j.wiebbelling@sintlucasedu.nl>
2023-06-12 10:39:00 +0300

    NPC talks now

--- Assets/scripts/Julia/NPC.cs
+++ Assets/scripts/Julia/NPC.cs
@@ -1,10 +1,21 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class NPC : MonoBehaviour
 {
+    public GameObject objectToEnable;
+
     public void Interact()
     {
         Debug.Log("NPC: Hi!");
-        // You can add more behavior or dialogue here for the NPC
+        // Enable the object for a few seconds
+        objectToEnable.SetActive(true);
+        StartCoroutine(DisableObjectAfterDelay(objectToEnable, 3f)); // Change the duration (3f) as needed
+    }
+    private IEnumerator DisableObjectAfterDelay(GameObject obj, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        obj.SetActive(false);
     }
 }
