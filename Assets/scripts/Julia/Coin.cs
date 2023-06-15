af8c390cc8012daa9af71f0122ee8a738183a21f
j.wiebbelling <j.wiebbelling@sintlucasedu.nl>
2023-06-15 14:20:21 +0300

    Coins and counter are done

--- Assets/scripts/Julia/Coin.cs
+++ Assets/scripts/Julia/Coin.cs
@@ -2,16 +2,14 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
-    public int coinValue = 1;  // Value to increase the counter by
+    public int coinValue = 100;  // Value to increase the counter by
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            // Increase the counter
-            //Counter.instance.IncreaseCounter(coinValue);
+            Counter.instance.IncreaseCounter(coinValue);
 
-            // Destroy the coin object
             Destroy(gameObject);
         }
     }
